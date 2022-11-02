cb5cadd1eac0cea203acfd3ded8008315a54c7a0
Desire1990 <desnduwim26@gmail.com>
2022-11-02 13:01:24 +0200

    update

--- GestioLabo/urls.py
+++ GestioLabo/urls.py
@@ -2,12 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include, re_path  
-# from . import views
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('auth/', include('rest_auth.urls')),
     path('api/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
-    # re_path(r'^.*$', views.index),     
+    re_path(r'^.*$', views.index),     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
