c32171c2993eab3110d8cd49aea373e8e6f23a1c
Desire1990 <desnduwim26@gmail.com>
2022-11-02 13:35:14 +0200

    great

--- api/urls.py
+++ api/urls.py
@@ -2,11 +2,9 @@ from django.urls import path, include
 from rest_framework import routers
 from .views import *
 from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
-# <<<<<<< HEAD
-# =======
-# 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
-# >>>>>>> origin/main
-# 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
+
+# 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
+
 router = routers.DefaultRouter()
 router.register("user",UserViewSet)
 router.register("utilisateur",UtilisateurViewset)
