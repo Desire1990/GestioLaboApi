4a25b291a97272061af49d15509c7f9396c898ec
Desire1990 <desnduwim26@gmail.com>
2023-01-12 16:36:50 +0200

    security rsa integration on db

--- api/rsa.py
+++ api/rsa.py
@@ -1,36 +1,36 @@
-#pip install pycryptodomex
-from Crypto.Cipher import PKCS1_OAEP
-from Crypto.PublicKey import RSA
+# #pip install pycryptodomex
+# from Crypto.Cipher import PKCS1_OAEP
+# from Crypto.PublicKey import RSA
 
 
-new_key = RSA.generate(2048)
+# new_key = RSA.generate(4096)
 
-private_key = new_key.exportKey("PEM")
-public_key = new_key.publickey().exportKey("PEM")
+# private_key = new_key.exportKey("PEM")
+# public_key = new_key.publickey().exportKey("PEM")
 
-# print(private_key.decode("utf-8"))
-fd = open("private_key.pem", "wb")
-fd.write(private_key)
-fd.close()
+# # print(private_key.decode("utf-8"))
+# fd = open("private_key.pem", "wb")
+# fd.write(private_key)
+# fd.close()
 
-# print(public_key.decode("utf-8"))
-fd = open("public_key.pem", "wb")
-fd.write(public_key)
-fd.close()
+# # print(public_key.decode("utf-8"))
+# fd = open("public_key.pem", "wb")
+# fd.write(public_key)
+# fd.close()
 
-message = input('Enter what you need to be crypted!!! \n\n')
+# message = input('Enter what you need to be crypted!!! \n\n')
 
 
-# encryption
-pubkey = RSA.import_key(open('public_key.pem').read())
-cipher_pub = PKCS1_OAEP.new(pubkey)
-ciphertext = cipher_pub.encrypt(str(message).encode('utf-8'))
-print(ciphertext)
-print('\n\n')
+# # encryption
+# pubkey = RSA.import_key(open('public_key.pem').read())
+# cipher_pub = PKCS1_OAEP.new(pubkey)
+# ciphertext = cipher_pub.encrypt(str(message).encode('utf-8'))
+# print(ciphertext)
+# print('\n\n')
 
-# decryption
-privkey = RSA.import_key(open('private_key.pem').read())
-cipher_priv = PKCS1_OAEP.new(privkey)
-plaintext = cipher_priv.decrypt(ciphertext)
-print (plaintext.decode("utf-8"))
+# # decryption
+# privkey = RSA.import_key(open('private_key.pem').read())
+# cipher_priv = PKCS1_OAEP.new(privkey)
+# plaintext = cipher_priv.decrypt(ciphertext)
+# print (plaintext.decode("utf-8"))
 
